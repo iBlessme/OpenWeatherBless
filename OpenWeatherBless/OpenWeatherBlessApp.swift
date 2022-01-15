154c01eb718787a97b4290883b348a8b502a4aec
iblessme <vldikk12@gmail.com>
2022-01-15 15:16:01 +0300

    Initial Commit

---
+++ OpenWeatherBless/OpenWeatherBlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherBlessApp.swift
+//  OpenWeatherBless
+//
+//  Created by iBlessme on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherBlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
